680c110fecf6179a53ff49f984242714a6d1085f
Jackey Lea <1768478912@qq.com>
2024-01-15 22:41:20 +0800

    添加软件图标

--- texaspoker/main.cpp
+++ texaspoker/main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowTitle(QObject::tr("Logger"));
+    w.setWindowIcon(QIcon(":/resources/images/poker.svg"));
     w.show();
     return a.exec();
 }
