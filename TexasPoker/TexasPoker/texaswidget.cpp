f034f59112bfd5a64c6d9f2a62a1096ebfe99649
Jackey Lea <1768478912@qq.com>
2024-08-19 21:24:46 +0800

    根据花色和数字显示对应扑克牌

--- TexasPoker/TexasPoker/texaswidget.cpp
+++ TexasPoker/TexasPoker/texaswidget.cpp
@@ -4,11 +4,37 @@
 TexasWidget::TexasWidget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::TexasWidget)
+    ,decor(0)
+    ,num(2)
 {
     ui->setupUi(this);
+
+    //设置默认图片
+    ui->labelImg->setPixmap(QPixmap(":/resources/images/back.svg"));
 }
 
 TexasWidget::~TexasWidget()
 {
     delete ui;
 }
+
+void TexasWidget::displayCard()
+{
+    QString path = QString(":/resources/images/%1%2.svg").arg(decor).arg(num);
+    ui->labelImg->setPixmap(QPixmap(path));
+}
+
+
+void TexasWidget::on_comboBoxNum_currentTextChanged(const QString &txt)
+{
+    num = txt.toInt();
+    displayCard();
+}
+
+
+void TexasWidget::on_comboBoxDecor_currentTextChanged(const QString &txt)
+{
+    decor = txt.toInt();
+    displayCard();
+}
+
--- TexasPoker/TexasPoker/texaswidget.h
+++ TexasPoker/TexasPoker/texaswidget.h
@@ -17,7 +17,17 @@ public:
     TexasWidget(QWidget *parent = nullptr);
     ~TexasWidget();
 
+    void displayCard();
+
+private slots:
+    void on_comboBoxNum_currentTextChanged(const QString &txt);
+
+    void on_comboBoxDecor_currentTextChanged(const QString &txt);
+
 private:
     Ui::TexasWidget *ui;
+
+    int decor;
+    int num;
 };
 #endif // TEXASWIDGET_H
