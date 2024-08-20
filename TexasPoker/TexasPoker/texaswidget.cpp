7099ecb5dd3638e6ceb455927834775661c3128d
Jackey Lea <1768478912@qq.com>
2024-08-20 17:30:53 +0800

    判断5张牌牌型

--- TexasPoker/TexasPoker/texaswidget.cpp
+++ TexasPoker/TexasPoker/texaswidget.cpp
@@ -38,5 +38,14 @@ void TexasWidget::on_btn_Gen_clicked()
         QString path = QString(":/resources/images/%1%2.svg").arg(c.CardDecor).arg(c.CardNum);
         label[i]->setPixmap(QPixmap(path));
     }
-}
 
+    //判断牌型
+    Cards cs;
+    cs.card[0]=cards[0];
+    cs.card[1]=cards[1];
+    cs.card[2]=cards[2];
+    cs.card[3]=cards[3];
+    cs.card[4]=cards[4];
+    BrandType bt = checkBranchType(cs);
+    ui->lineEditBrand->setText(getBrandType(bt));
+}
