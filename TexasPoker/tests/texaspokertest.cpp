9258c78a2c864f8b6be9c5d361bc6571f65570d6
Jackey Lea <1768478912@qq.com>
2024-08-21 13:09:27 +0800

    添加测试类

---
+++ TexasPoker/tests/texaspokertest.cpp
@@ -0,0 +1,3 @@
+#include "texaspokertest.h"
+
+TexasPokerTest::TexasPokerTest() {}
---
+++ TexasPoker/tests/texaspokertest.h
@@ -0,0 +1,13 @@
+#ifndef TEXASPOKERTEST_H
+#define TEXASPOKERTEST_H
+
+#include <QObject>
+
+class TexasPokerTest
+{
+    Q_OBJECT
+public:
+    TexasPokerTest();
+};
+
+#endif // TEXASPOKERTEST_H
