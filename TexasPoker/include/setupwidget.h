1fc1d00ff4555ccaa31546874ca2958da8e8b14b
Jackey Lea <1768478912@qq.com>
2025-01-08 16:33:13 +0800

    更新函数说明

--- TexasPoker/include/setupwidget.h
+++ TexasPoker/include/setupwidget.h
@@ -24,14 +24,20 @@ public:
     ~SetupWidget();
 
 private slots:
+    /// 加入牌桌加入游戏
+    /// \brief on_btnJoin_clicked
+    ///
     void on_btnJoin_clicked();
 
+    /// 取消操作，退出游戏
+    /// \brief on_btnCance_clicked
+    ///
     void on_btnCance_clicked();
 
 private:
     Ui::SetupWidget *ui;
 
-    TexasWidget *m_pTexasWidget;
+    TexasWidget *m_pTexasWidget;//牌桌界面
 };
 
 #endif // SETUPWIDGET_H
